5251d77f5fad51434d68385ffd89eaa6f0252a39
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-06-12 20:00:48 +0200

    Minor changes to unit_test_create_post.py

--- unit_test_create_post.py
+++ unit_test_create_post.py
@@ -18,7 +18,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def tearDown(self):
         with app.app_context():
-            posts = Post.query.filter(Post.user_id == 4).all()
+            posts = Post.query.filter(Post.user_id == self.user_id).all()
             for post in posts:
                 db.session.delete(post)
                 db.session.commit()
@@ -29,7 +29,7 @@ class FlaskTestCase(unittest.TestCase):
             "body": "It was good",
             "category": "Breakfast"
         }
-        response = self.app.post("/create_post/4", json=data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=data)
         print("\n---------- TEST WITH VALID DATA ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 201)
@@ -39,7 +39,7 @@ class FlaskTestCase(unittest.TestCase):
             "body": "It was good",
             "category": "Breakfast"
         }
-        response = self.app.post("/create_post/4", json=data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=data)
         print("\n---------- TEST WITH NO TITLE FIELD ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 400)
@@ -49,7 +49,7 @@ class FlaskTestCase(unittest.TestCase):
             "title": "I had breakfast today",
             "category": "Breakfast"
         }
-        response = self.app.post("/create_post/4", json=data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=data)
         print("\n---------- TEST WITH NO BODY FIELD ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 400)
@@ -59,7 +59,7 @@ class FlaskTestCase(unittest.TestCase):
             "title": "I had breakfast today",
             "body": "It was good"
         }
-        response = self.app.post("/create_post/4", json=data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=data)
         print("\n---------- TEST WITH NO CATEGORY FIELD ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 400)
@@ -82,20 +82,8 @@ class FlaskTestCase(unittest.TestCase):
             "body": "It was good",
             "category": "Breakfast"
         }
-        response = self.app.post("/create_post", json=data)
+        response = self.app.post(f"/create_post", json=data)
         print("\n---------- TEST WITH NO USER ID ----------\n")
-        print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
-        self.assertEqual(response.status_code, 404)
-
-    def test_with_invalid_user(self):
-        data = {
-            "title": "I had breakfast today",
-            "body": "It was good",
-            "category": "Breakfast"
-        }
-        invalid_user_id = -1
-        response = self.app.post(f"/create_post/{invalid_user_id}", json=data)
-        print("\n---------- TEST WITH INVALID USER ID ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 404)
 
@@ -106,7 +94,7 @@ class FlaskTestCase(unittest.TestCase):
             "body": "!§&$/%(§%)§%§%=&)/$It was good!§&$/%(§%)§%§%=&)/$",
             "category": "!§&$/%(§%)§%§%=&)/$Breakfast!§&$/%(§%)§%§%=&)/$"
         }
-        response = self.app.post(f"/create_post/4", json=data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=data)
         print("\n---------- TEST WITH SPECIAL CHARACTERS IN FIELDS ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 201)
@@ -119,13 +107,13 @@ class FlaskTestCase(unittest.TestCase):
             "category": "Breakfast"
             }
         ]
-        response = self.app.post(f"/create_post/4", json=invalid_data)
+        response = self.app.post(f"/create_post/{self.user_id}", json=invalid_data)
         print("\n---------- TEST WITH INVALID DATA ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 400)
 
     def test_with_no_data(self):
-        response = self.app.post(f"/create_post/4", json=None)
+        response = self.app.post(f"/create_post/{self.user_id}", json=None)
         print("\n---------- TEST WITH NO DATA ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 415)
@@ -134,7 +122,7 @@ class FlaskTestCase(unittest.TestCase):
         invalid_title = True
         invalid_body = 123
         invalid_category = None
-        response = self.app.post(f"/create_post/4", json={"title": invalid_title, "body": invalid_body, "category": invalid_category})
+        response = self.app.post(f"/create_post/{self.user_id}", json={"title": invalid_title, "body": invalid_body, "category": invalid_category})
         print("\n---------- TEST WITH INVALID FIELDS ----------\n")
         print(f"Response Code: {response.status_code}\nResponse: {response.get_data(as_text=True)}")
         self.assertEqual(response.status_code, 400)
